e44b055e89a109040607fa463952406ad96901b6
Patrick Masselink <patrick.masselink@prismtech.com>
2017-04-12 17:56:44 +0200

    CHAM-47 - Add CMake abstraction package file

---
+++ cmake/modules/FindAbstraction.cmake
@@ -0,0 +1,13 @@
+message(STATUS "Loading platform abstraction package")
+
+add_library(Abstraction INTERFACE)
+
+# Link with the platform-specific threads library that find_package provides us
+set(CMAKE_THREAD_PREFER_PTHREAD TRUE)
+find_package(Threads REQUIRED)
+target_link_libraries(Abstraction INTERFACE Threads::Threads)
+
+if(WIN32)
+  # Link with WIN32 core-libraries
+  target_link_libraries(Abstraction INTERFACE wsock32 ws2_32 Iphlpapi)
+endif()
