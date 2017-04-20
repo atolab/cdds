e9a3aecb6029c0dba3dbf10d0ac716703999698a
Jeroen Koekkoek <jeroen.koekkoek@prismtech.com>
2017-04-20 17:21:52 +0200

    CHAM-28 - Fix detection of clock_gettime

--- cmake/modules/FindGetTime.cmake
+++ cmake/modules/FindGetTime.cmake
@@ -3,12 +3,12 @@ add_library(GetTime INTERFACE)
 include(CheckLibraryExists)
 
 # First check whether libc has clock_gettime
-check_library_exists(c clock_gettime "" HAVE_CLOCK_GETTIME)
+check_library_exists(c clock_gettime "" HAVE_CLOCK_GETTIME_IN_C)
 
-if(NOT HAVE_CLOCK_GETTIME)
+if(NOT HAVE_CLOCK_GETTIME_IN_C)
   # Before glibc 2.17, clock_gettime was in librt
-  check_library_exists(rt clock_gettime "time.h" HAVE_CLOCK_GETTIME)
-  if (HAVE_CLOCK_GETTIME)
+  check_library_exists(rt clock_gettime "time.h" HAVE_CLOCK_GETTIME_IN_RT)
+  if (HAVE_CLOCK_GETTIME_IN_RT)
     target_link_libraries(GetTime INTERFACE rt)
   endif()
 endif()
