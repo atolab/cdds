04c32f48036610e8820967d7754004c16a59ff63
Maurits de Jong <maurits.dejong@prismtech.com>
2017-04-21 15:01:09 +0200

    CHAM-47 - Added imported targets for IP Helper API and Winsock2 and made FindGetTime conditional on running on Unix (but not APPLE).

--- cmake/modules/FindAbstraction.cmake
+++ cmake/modules/FindAbstraction.cmake
@@ -5,10 +5,14 @@ add_library(Abstraction INTERFACE)
 # Link with the platform-specific threads library that find_package provides us
 set(CMAKE_THREAD_PREFER_PTHREAD TRUE)
 find_package(Threads REQUIRED)
-find_package(GetTime REQUIRED)
-target_link_libraries(Abstraction INTERFACE Threads::Threads GetTime)
+target_link_libraries(Abstraction INTERFACE Threads::Threads)
 
 if(WIN32)
   # Link with WIN32 core-libraries
-  target_link_libraries(Abstraction INTERFACE wsock32 ws2_32 Iphlpapi)
+  find_package(Winsock2 REQUIRED)
+  find_package(IPHelper REQUIRED)
+  target_link_libraries(Abstraction INTERFACE Winsock2 IPHelper)
+elseif(UNIX AND NOT APPLE)
+  find_package(GetTime REQUIRED)
+  target_link_libraries(Abstraction INTERFACE GetTime)
 endif()
