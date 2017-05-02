4f92f1986d7b872c23f967725e53865789ec7e3c
Martin Bremmer <martin.bremmer@prismtech.com>
2017-05-02 17:43:30 +0200

    CHAM-36: Added coverage support.

---
+++ cmake/scripts/CoveragePreCobertura.cmake
@@ -0,0 +1,18 @@
+#
+# This script assumes that it is called before all tests are run and gcov results are available.
+# It can be used to setup the environment needed to get proper Cobertura coverage results.
+#
+# Example usage:
+# $ cmake -DCOVERAGE_SETTINGS=<cham bld>/CoverageSettings.cmake -P <cham src>/cmake/scripts/CoveragePreCobertura.cmake
+# $ ctest -T test
+# $ ctest -T coverage
+# $ ctest -DCOVERAGE_SETTINGS=<cham bld>/CoverageSettings.cmake -P <cham src>/cmake/scripts/CoveragePostCobertura.cmake
+# If you start the scripts while in <cham bld> then you don't have to provide the COVERAGE_SETTINGS file.
+#
+cmake_minimum_required(VERSION 3.5)
+
+#
+# Nothing to do really.
+# This is just added to provide consistency between Coverage scripts.
+#
+
