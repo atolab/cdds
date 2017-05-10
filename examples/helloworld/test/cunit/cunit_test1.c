e46fdd56ac97171373c951e6adc5c0e3d7f23737
Maurits de Jong <maurits.dejong@prismtech.com>
2017-05-11 00:11:58 +0200

    CHAM-92 - Added generic CUnit runner.
    Also modified existing cunit example testcase to use the runner.

--- examples/helloworld/test/cunit/cunit_test1.c
+++ examples/helloworld/test/cunit/cunit_test1.c
@@ -1,68 +1,46 @@
 #include "dds.h"
-#include "CUnit/CUnit.h"
-#include "CUnit/Automated.h"
+#include "cunitrunner/runner.h"
 
-/* Pointer to the file used by the tests. */
-static FILE* temp_file = NULL;
-static int ac;
-static char **av;
+void testcase_1 (void)
+{
+    dds_entity_t participant;
+    int status;
 
+    status = dds_init(0, NULL);
+    CU_ASSERT(status == DDS_RETCODE_OK);
+    DDS_ERR_CHECK(status, DDS_CHECK_REPORT | DDS_CHECK_EXIT);
+    status = dds_participant_create(&participant, DDS_DOMAIN_DEFAULT, NULL, NULL);
+    CU_ASSERT(status == DDS_RETCODE_OK);
+    DDS_ERR_CHECK(status, DDS_CHECK_REPORT | DDS_CHECK_EXIT);
 
-void testcase_1(void) {
+    dds_entity_delete(participant);
 
-  dds_entity_t participant;
-  int status;
-
-  status = dds_init (ac, av);
-  CU_ASSERT(status == DDS_RETCODE_OK);
-  DDS_ERR_CHECK (status, DDS_CHECK_REPORT | DDS_CHECK_EXIT);
-  status = dds_participant_create (&participant, DDS_DOMAIN_DEFAULT, NULL, NULL);
-  CU_ASSERT(status == DDS_RETCODE_OK);
-  DDS_ERR_CHECK (status, DDS_CHECK_REPORT | DDS_CHECK_EXIT);
-
-  dds_entity_delete (participant);
-
-  dds_fini ();
+    dds_fini();
 }
-
-
 
 int main (int argc, char *argv[])
 {
+    CU_pSuite examples;
 
-   CU_pSuite pSuite = NULL;
+    if (runner_init(argc, argv)) {
+        goto err_init;
+    }
 
-   ac = argc;
-   av = argv;
+    /* add a suite to the registry */
+    if ((examples = CU_add_suite("examples/helloworld", NULL, NULL)) == NULL) {
+        goto err;
+    }
 
-   /* initialize the CUnit test registry */
-   if (CUE_SUCCESS != CU_initialize_registry()) {
-      return CU_get_error();
-   }
+    /* add test cases to the test suite */
+    if (CU_add_test(examples, "dds_participant_create", testcase_1) == NULL) {
+        goto err;
+    }
 
-   /* add a suite to the registry */
-   pSuite = CU_add_suite("HelloWorld test suite", NULL, NULL);
-   if (NULL == pSuite) {
-      CU_cleanup_registry();
-      return CU_get_error();
-   }
+    runner_run();
 
-   /* add test cases to the test suite */ 
-   if (NULL == CU_add_test(pSuite, "dds_participant_create", testcase_1)) {
-      CU_cleanup_registry();
-      return CU_get_error();
-   }
-
-
-   /* Run all tests using the CUnit Automated interface */
-   CU_set_output_filename ("cunit");
-   CU_list_tests_to_file ();
-   CU_automated_run_tests ();
- 
-   /* cleanup registry */
-   CU_cleanup_registry();
-
-  /* ctest requires the test executable to return 0 when succuss, non-null when fail */
-  return CU_get_error();
+err:
+    runner_fini();
+err_init:
+    return CU_get_error();
 }
 
