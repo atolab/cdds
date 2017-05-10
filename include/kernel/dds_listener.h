107695f31179794582053a537068d2fbbbb2dd29
Martin Bremmer <martin.bremmer@prismtech.com>
2017-05-10 16:07:46 +0200

    CHAM-88: Fixed Windows build problem.

--- include/kernel/dds_listener.h
+++ include/kernel/dds_listener.h
@@ -1,6 +1,7 @@
 #ifndef _DDS_LISTENER_H_
 #define _DDS_LISTENER_H_
 
+#include "dds/dds_public_impl.h"
 #include "kernel/dds_types.h"
 
 #if defined (__cplusplus)
@@ -8,12 +9,43 @@ extern "C" {
 #endif
 
 void dds_listener_merge (dds_listener_t dst, const dds_listener_t src, dds_entity_kind_t kind);
-void dds_listener_get_unl (dds_entity_t e, dds_listener_t l);
-void dds_listener_set_unl (dds_entity_t e, const dds_listener_t l);
-void dds_listener_delete (_In_ _Post_invalid_ dds_listener_t * restrict listener);
+void dds_listener_delete (_In_ _Post_invalid_ dds_listener_t __restrict listener);
+void dds_listener_lock (_In_ _Post_invalid_ dds_listener_t __restrict listener);
+void dds_listener_unlock (_In_ _Post_invalid_ dds_listener_t __restrict listener);
 
-void dds_listener_lock (_In_ _Post_invalid_ dds_listener_t * restrict listener);
-void dds_listener_unlock (_In_ _Post_invalid_ dds_listener_t * restrict listener);
+
+/* TODO: Merge the CHAM-65 Listener stuff. */
+typedef void (*dds_on_inconsistent_topic_fn) (dds_entity_t topic, const dds_inconsistent_topic_status_t status);
+typedef void (*dds_on_liveliness_lost_fn) (dds_entity_t writer, const dds_liveliness_lost_status_t status);
+typedef void (*dds_on_offered_deadline_missed_fn) (dds_entity_t writer, const dds_offered_deadline_missed_status_t status);
+typedef void (*dds_on_offered_incompatible_qos_fn) (dds_entity_t writer, const dds_offered_incompatible_qos_status_t status);
+typedef void (*dds_on_data_on_readers_fn) (dds_entity_t subscriber);
+typedef void (*dds_on_sample_lost_fn) (dds_entity_t reader, const dds_sample_lost_status_t status);
+typedef void (*dds_on_data_available_fn) (dds_entity_t reader);
+typedef void (*dds_on_sample_rejected_fn) (dds_entity_t reader, const dds_sample_rejected_status_t status);
+typedef void (*dds_on_liveliness_changed_fn) (dds_entity_t reader, const dds_liveliness_changed_status_t status);
+typedef void (*dds_on_requested_deadline_missed_fn) (dds_entity_t reader, const dds_requested_deadline_missed_status_t status);
+typedef void (*dds_on_requested_incompatible_qos_fn) (dds_entity_t reader, const dds_requested_incompatible_qos_status_t status);
+typedef void (*dds_on_publication_matched_fn) (dds_entity_t writer, const dds_publication_matched_status_t  status);
+typedef void (*dds_on_subscription_matched_fn) (dds_entity_t reader, const dds_subscription_matched_status_t  status);
+
+typedef struct c99_listener_cham65 {
+    dds_on_inconsistent_topic_fn on_inconsistent_topic;
+    dds_on_liveliness_lost_fn on_liveliness_lost;
+    dds_on_offered_deadline_missed_fn on_offered_deadline_missed;
+    dds_on_offered_incompatible_qos_fn on_offered_incompatible_qos;
+    dds_on_data_on_readers_fn on_data_on_readers;
+    dds_on_sample_lost_fn on_sample_lost;
+    dds_on_data_available_fn on_data_available;
+    dds_on_sample_rejected_fn on_sample_rejected;
+    dds_on_liveliness_changed_fn on_liveliness_changed;
+    dds_on_requested_deadline_missed_fn on_requested_deadline_missed;
+    dds_on_requested_incompatible_qos_fn on_requested_incompatible_qos;
+    dds_on_publication_matched_fn on_publication_matched;
+    dds_on_subscription_matched_fn on_subscription_matched;
+    os_mutex m_mutex;
+} c99_listener_cham65_t;
+
 
 #if defined (__cplusplus)
 }
--- src/kernel/dds_listener.c
+++ src/kernel/dds_listener.c
@@ -1,134 +1,17 @@
+
 #include <assert.h>
 #include "kernel/dds_listener.h"
 
-void dds_listener_get (dds_entity_t e, dds_listener_t l)
-{
-  os_mutexLock (&e->m_mutex);
-  dds_listener_get_unl (e, l);
-  os_mutexUnlock (&e->m_mutex);
-}
-
-void dds_listener_get_unl (dds_entity_t e, dds_listener_t l)
-{
-  assert (e);
-  assert (l);
-
-  switch (e->m_kind)
-  {
-    case DDS_TYPE_TOPIC: *((dds_topiclistener_t*) l) = ((dds_topic*) e)->m_listener; break;
-    case DDS_TYPE_PARTICIPANT: *((dds_participantlistener_t*) l) = ((dds_participant*) e)->m_listener; break;
-    case DDS_TYPE_READER: *((dds_readerlistener_t*) l) = ((dds_reader*) e)->m_listener; break;
-    case DDS_TYPE_WRITER: *((dds_writerlistener_t*) l) = ((dds_writer*) e)->m_listener; break;
-    case DDS_TYPE_SUBSCRIBER: *((dds_subscriberlistener_t*) l) = ((dds_subscriber*) e)->m_listener; break;
-    case DDS_TYPE_PUBLISHER: *((dds_publisherlistener_t*) l) = ((dds_publisher*) e)->m_listener; break;
-  }
-}
-
-void dds_listener_set (dds_entity_t e, const dds_listener_t l)
-{
-  os_mutexLock (&e->m_mutex);
-  dds_listener_set_unl (e, l);
-  os_mutexUnlock (&e->m_mutex);
-}
-
-void dds_listener_set_unl (dds_entity_t e, const dds_listener_t l)
-{
-  assert (e);
-  assert (l);
-
-  switch (e->m_kind)
-  {
-    case DDS_TYPE_TOPIC: ((dds_topic*) e)->m_listener = *(dds_topiclistener_t*) l; break;
-    case DDS_TYPE_PARTICIPANT: ((dds_participant*) e)->m_listener = *(dds_participantlistener_t*) l; break;
-    case DDS_TYPE_READER: ((dds_reader*) e)->m_listener = *(dds_readerlistener_t*) l; break;
-    case DDS_TYPE_WRITER: ((dds_writer*) e)->m_listener = *(dds_writerlistener_t*) l; break;
-    case DDS_TYPE_SUBSCRIBER: ((dds_subscriber*) e)->m_listener = *(dds_subscriberlistener_t*) l; break;
-    case DDS_TYPE_PUBLISHER: ((dds_publisher*) e)->m_listener = *(dds_publisherlistener_t*) l; break;
-  }
-}
-
-/*
-  dds_listener_merge: Merge "dst" listener functions with those of "src". Only merge function
-  pointers that are not already set in "dst".
-*/
-
-void dds_listener_merge (dds_listener_t dst, const dds_listener_t src, dds_entity_kind_t kind)
-{
-  assert (dst);
-  assert (src);
-
-#define DDS_LMERGE(n) if (d->n == NULL) d->n = s->n
-
-  switch (kind)
-  {
-    case DDS_TYPE_TOPIC:
-    {
-      dds_topiclistener_t * d = (dds_topiclistener_t*) dst;
-      dds_topiclistener_t * s = (dds_topiclistener_t*) src;
-      DDS_LMERGE (on_inconsistent_topic);
-      break;
-    }
-    case DDS_TYPE_PARTICIPANT:
-    {
-      dds_participantlistener_t * d = (dds_participantlistener_t*) dst;
-      dds_participantlistener_t * s = (dds_participantlistener_t*) src;
-      dds_listener_merge (&d->topiclistener, &s->topiclistener, DDS_TYPE_TOPIC);
-      dds_listener_merge (&d->publisherlistener, &s->publisherlistener, DDS_TYPE_PUBLISHER);
-      dds_listener_merge (&d->subscriberlistener, &s->subscriberlistener, DDS_TYPE_SUBSCRIBER);
-      break;
-    }
-    case DDS_TYPE_READER:
-    {
-      dds_readerlistener_t * d = (dds_readerlistener_t*) dst;
-      dds_readerlistener_t * s = (dds_readerlistener_t*) src;
-      DDS_LMERGE (on_requested_deadline_missed);
-      DDS_LMERGE (on_requested_incompatible_qos);
-      DDS_LMERGE (on_sample_rejected);
-      DDS_LMERGE (on_liveliness_changed);
-      DDS_LMERGE (on_data_available);
-      DDS_LMERGE (on_subscription_matched);
-      DDS_LMERGE (on_sample_lost);
-      break;
-    }
-    case DDS_TYPE_WRITER:
-    {
-      dds_writerlistener_t * d = (dds_writerlistener_t*) dst;
-      dds_writerlistener_t * s = (dds_writerlistener_t*) src;
-      DDS_LMERGE (on_offered_deadline_missed);
-      DDS_LMERGE (on_offered_incompatible_qos);
-      DDS_LMERGE (on_liveliness_lost);
-      DDS_LMERGE (on_publication_matched);
-      break;
-    }
-    case DDS_TYPE_SUBSCRIBER:
-    {
-      dds_subscriberlistener_t * d = (dds_subscriberlistener_t*) dst;
-      dds_subscriberlistener_t * s = (dds_subscriberlistener_t*) src;
-      DDS_LMERGE (on_data_readers);
-      dds_listener_merge (&d->readerlistener, &s->readerlistener, DDS_TYPE_READER);
-      break;
-    }
-    case DDS_TYPE_PUBLISHER:
-    {
-      dds_publisherlistener_t * d = (dds_publisherlistener_t*) dst;
-      dds_publisherlistener_t * s = (dds_publisherlistener_t*) src;
-      dds_listener_merge (&d->writerlistener, &s->writerlistener, DDS_TYPE_WRITER);
-      break;
-    }
-  }
-
-#undef DDS_LMERGE
-}
 
 
 
 /* TODO: Merge the CHAM-65 Listener stuff. */
-
+typedef void (*dds_on_any_fn) (); /**< Empty parameter list on purpose; should be assignable without cast to all of the above. @todo check with an actual compiler; I'm a sloppy compiler */
 #define DDS_LUNSET ((dds_on_any_fn)1) /**< Callback indicating a callback isn't set */
-dds_listener_cham65_t*
+dds_listener_t
 dds_listener_create(void)
 {
-    dds_listener_cham65_t *l = dds_alloc(sizeof(*l));
+    dds_listener_cham65_t *l = dds_alloc(sizeof(dds_listener_cham65_t));
     l->on_data_available = DDS_LUNSET;
     l->on_data_on_readers = DDS_LUNSET;
     l->on_inconsistent_topic = DDS_LUNSET;
@@ -142,26 +25,37 @@ dds_listener_create(void)
     l->on_sample_lost = DDS_LUNSET;
     l->on_sample_rejected = DDS_LUNSET;
     l->on_subscription_matched = DDS_LUNSET;
-    //os_mutexInit (&l->m_mutex);
-    return l;
-}
-
-void dds_listener_lock (_In_ _Post_invalid_ dds_listener_t * restrict l)
-{
-    c99_listener_cham65_t *listener = (c99_listener_cham65_t*)l;
-    assert(listener);
-    //os_mutexLock (&listener->m_mutex);
-}
-
-void dds_listener_unlock (_In_ _Post_invalid_ dds_listener_t * restrict l)
-{
-    c99_listener_cham65_t *listener = (c99_listener_cham65_t*)l;
-    assert(listener);
-    //os_mutexLock (&listener->m_mutex);
+    os_mutexInit(&l->m_mutex);
+    return (dds_listener_t*)l;
 }
 
 void
-dds_listener_copy(_Inout_ dds_listener_cham65_t * restrict dst, _In_ const dds_listener_cham65_t * restrict src)
+dds_listener_delete(_In_ _Post_invalid_ dds_listener_t __restrict l)
+{
+    c99_listener_cham65_t *listener = (c99_listener_cham65_t*)l;
+    if (listener) {
+        os_mutexDestroy(&listener->m_mutex);
+        dds_free(listener);
+    }
+}
+
+void dds_listener_lock (_In_ _Post_invalid_ dds_listener_t __restrict l)
+{
+    c99_listener_cham65_t *listener = (c99_listener_cham65_t*)l;
+    assert(listener);
+    os_mutexLock (&listener->m_mutex);
+}
+
+/* TODO: Lock/unlock listener when setting callback functions. */
+void dds_listener_unlock (_In_ _Post_invalid_ dds_listener_t __restrict l)
+{
+    c99_listener_cham65_t *listener = (c99_listener_cham65_t*)l;
+    assert(listener);
+    os_mutexLock (&listener->m_mutex);
+}
+
+void
+dds_listener_copy(_Inout_ dds_listener_t __restrict dst, _In_ const dds_listener_t __restrict src)
 {
     if (src && dst) {
         const c99_listener_cham65_t *srcl = src;
