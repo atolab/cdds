5c7899c94abaf0549c5ad02f223f7237d6afe16c
Maurits de Jong <maurits.dejong@prismtech.com>
2017-06-07 22:26:34 +0200

    CHAM-155 - Shot at fixing VxWorks kernel build.

--- include/os/vxworks/os_platform.h
+++ include/os/vxworks/os_platform.h
@@ -14,19 +14,62 @@
 /* inttypes.h does not exist in VxWorks DKM */
 #include <st_inttypes.h>
 #include <cafe/inttypes.h>
+
+/* The above inttypes includes don't seem to define uintmax_t &c. */
 #ifdef _WRS_CONFIG_LP64 /* Used in cafe/inttypes.h too */
 #define _PFX_64 "l"
+typedef long int                intmax_t;
+typedef unsigned long int       uintmax_t;
 #else
 #define _PFX_64 "ll"
+typedef long long int           intmax_t;
+typedef unsigned long long int  uintmax_t;
 #endif
 
-/* FIXME: Not a complete replacement for inttypes.h (yet) */
-#define PRIu32 "u"
-#define PRIu64 _PFX_64 "u"
-#define PRId32 "d"
-#define PRId64 _PFX_64 "d"
-#define PRIi32 "i"
-#define PRIi64 _PFX_64 "i"
+/* FIXME: Not a complete replacement for inttypes.h (yet); no SCN/PRI?LEAST/FAST/etc */
+#define PRId8      "d"
+#define PRId16     "d"
+#define PRId32     "d"
+#define PRId64     _PFX_64 "d"
+
+#define PRIi8      "i"
+#define PRIi16     "i"
+#define PRIi32     "i"
+#define PRIi64     _PFX_64 "i"
+
+#define PRIo8      "o"
+#define PRIo16     "o"
+#define PRIo32     "o"
+#define PRIo64     _PFX_64 "o"
+
+#define PRIu8      "u"
+#define PRIu16     "u"
+#define PRIu32     "u"
+#define PRIu64     _PFX_64 "u"
+
+#define PRIx8      "x"
+#define PRIx16     "x"
+#define PRIx32     "x"
+#define PRIx64     _PFX_64 "x"
+
+#define PRIX8      "X"
+#define PRIX16     "X"
+#define PRIX32     "X"
+#define PRIX64     _PFX_64 "X"
+
+#define PRIdMAX    _PFX_64 "d"
+#define PRIiMAX    _PFX_64 "i"
+#define PRIoMAX    _PFX_64 "o"
+#define PRIuMAX    _PFX_64 "u"
+#define PRIxMAX    _PFX_64 "x"
+#define PRIXMAX    _PFX_64 "X"
+
+#define PRIdPTR    _PFX_64 "d"
+#define PRIiPTR    _PFX_64 "i"
+#define PRIoPTR    _PFX_64 "o"
+#define PRIuPTR    _PFX_64 "u"
+#define PRIxPTR    _PFX_64 "x"
+#define PRIXPTR    _PFX_64 "X"
 
 #define INFINITY infinity()
 #define NAN       ((float)(INFINITY * 0.0F))
