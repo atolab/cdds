c8684a764876374e1c3ba18fdc487bc3ce49f156
Jeroen Koekkoek <jeroen.koekkoek@prismtech.com>
2017-06-08 14:18:54 +0200

    CHAM-165 - Replace CMake list filter command
    Change-Id: I061bbac0ea15db453d13df29ef08b66437ed23c3

--- cmake/modules/Glob.cmake
+++ cmake/modules/Glob.cmake
@@ -2,6 +2,8 @@ function(glob variable extension)
   set(dirname "${CMAKE_CURRENT_SOURCE_DIR}")
 
   foreach(filename ${ARGN})
+    unset(filenames)
+
     if((NOT IS_ABSOLUTE "${filename}") AND
        (EXISTS "${dirname}/${filename}"))
       set(filename "${dirname}/${filename}")
@@ -10,12 +12,13 @@ function(glob variable extension)
     if(IS_DIRECTORY "${filename}")
       file(GLOB_RECURSE filenames "${filename}/*.${extension}")
     elseif(EXISTS "${filename}")
-      set(filenames "${filename}")
+      if("${filename}" MATCHES "\.${extension}$")
+        set(filenames "${filename}")
+      endif()
     else()
       message(FATAL_ERROR "File ${filename} does not exist")
     endif()
 
-    list(FILTER filenames INCLUDE REGEX "\.${extension}$")
     list(APPEND files ${filenames})
   endforeach()
 
