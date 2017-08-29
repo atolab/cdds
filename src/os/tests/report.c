0a5f19489d9338dc3c31fedcd7a103fbc6e52f46
Marcel Smit <marcel.smit@prismtech.com>
2017-08-29 12:18:29 +0200

    Fixed runtime issues on windows.

--- src/os/tests/report.c
+++ src/os/tests/report.c
@@ -29,14 +29,6 @@ void check_existence(os_result error_log_existence, os_result info_log_existence
   CU_ASSERT(os_access(info_file_name, OS_ROK) == info_log_existence);
 }
 
-// Start with a clean state before each individual test.
-void re_init()
-{
-  os_reportExit();
-  os_reportInit(true);
-  check_existence(os_resultFail, os_resultFail);
-}
-
 CUnit_Suite_Cleanup(os_report)
 {
   remove_logs();
@@ -46,7 +38,7 @@ CUnit_Suite_Cleanup(os_report)
 
 CUnit_Test(os_report, re_init)
 {
-  re_init();
+  os_reportInit(true);
 
   OS_INFO(OS_FUNCTION, 0, "os_report-info-test %d", __LINE__);
 
@@ -66,7 +58,7 @@ CUnit_Test(os_report, re_init)
 
 CUnit_Test(os_report, stack_critical)
 {
-  re_init();
+  os_reportInit(true);
 
   OS_REPORT_STACK();
 
@@ -79,12 +71,14 @@ CUnit_Test(os_report, stack_critical)
   // Since a critical is logged, the error log should be created
   check_existence(os_resultSuccess, os_resultFail);
 
+  os_reportExit();
+
   remove_logs();
 }
 
 CUnit_Test(os_report, stack_non_critical)
 {
-  re_init();
+  os_reportInit(true);
 
   OS_REPORT_STACK();
 
@@ -97,49 +91,59 @@ CUnit_Test(os_report, stack_non_critical)
   // Since a non critical is logged, the error log should not be created
   check_existence(os_resultFail, os_resultFail);
 
+  os_reportExit();
+
   remove_logs();
 }
 
 CUnit_Test(os_report, error_file_creation_critical)
 {
-  re_init();
+  os_reportInit(true);
 
   OS_CRITICAL(OS_FUNCTION, 0, "os_report-critical-test %d", __LINE__);
 
   check_existence(os_resultSuccess, os_resultFail);
+
+  os_reportExit();
 
   remove_logs();
 }
 
 CUnit_Test(os_report, error_file_creation_fatal)
 {
-  re_init();
+  os_reportInit(true);
 
   OS_FATAL(OS_FUNCTION, 0, "os_report-fatal-test %d", __LINE__);
 
   check_existence(os_resultSuccess, os_resultFail);
+
+  os_reportExit();
 
   remove_logs();
 }
 
 CUnit_Test(os_report, info_file_creation_warning)
 {
-  re_init();
+  os_reportInit(true);
 
   OS_WARNING(OS_FUNCTION, 0, "os_report-warning-test %d", __LINE__);
 
   check_existence(os_resultFail, os_resultSuccess);
+
+  os_reportExit();
 
   remove_logs();
 }
 
 CUnit_Test(os_report, info_file_creation_info)
 {
-  re_init();
+  os_reportInit(true);
 
   OS_INFO(OS_FUNCTION, 0, "os_report-info-test %d", __LINE__);
 
   check_existence(os_resultFail, os_resultSuccess);
+
+  os_reportExit();
 
   remove_logs();
 }
