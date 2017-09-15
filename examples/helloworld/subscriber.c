74841b455b26fc41f8fd3b011ccf9d9d45d36dc5
Martin Bremmer <martin.bremmer@prismtech.com>
2017-09-15 15:03:14 +0200

    CHAM-347: Helloworld builtin test.

--- examples/helloworld/subscriber.c
+++ examples/helloworld/subscriber.c
@@ -1,11 +1,14 @@
 #include "dds.h"
 #include "HelloWorldData.h"
+#include "dds_builtinTopics.h"
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 
 /* An array of one message (aka sample in dds terms) will be used. */
 #define MAX_SAMPLES 1
+
+#define CM_SAMPLES 10
 
 int main (int argc, char ** argv)
 {
@@ -18,9 +21,26 @@ int main (int argc, char ** argv)
     dds_return_t ret;
     dds_qos_t *qos;
 
+    dds_entity_t cm_topic;
+    dds_entity_t cm_reader;
+    void *cm_samples[CM_SAMPLES];
+    dds_sample_info_t cm_infos[CM_SAMPLES];
+    for (int i = 0; i < CM_SAMPLES; i++)
+    {
+        cm_samples[i] = DDS_CMParticipantBuiltinTopicData__alloc ();
+    }
+
     /* Create a Participant. */
     participant = dds_create_participant (DDS_DOMAIN_DEFAULT, NULL, NULL);
     DDS_ERR_CHECK (participant, DDS_CHECK_REPORT | DDS_CHECK_EXIT);
+
+    /* Create a Topic. */
+    cm_topic = dds_create_topic (participant, &DDS_CMParticipantBuiltinTopicData_desc, "CMParticipant", NULL, NULL);
+    DDS_ERR_CHECK (cm_topic, DDS_CHECK_REPORT | DDS_CHECK_EXIT);
+
+    /* Create a Reader. */
+    cm_reader = dds_create_reader (participant, cm_topic, NULL, NULL);
+    DDS_ERR_CHECK (cm_reader, DDS_CHECK_REPORT | DDS_CHECK_EXIT);
 
     /* Create a Topic. */
     topic = dds_create_topic (participant, &HelloWorldData_Msg_desc,
@@ -64,8 +84,27 @@ int main (int argc, char ** argv)
         }
     }
 
+    ret = dds_read (cm_reader, cm_samples, cm_infos, CM_SAMPLES, CM_SAMPLES);
+    DDS_ERR_CHECK (ret, DDS_CHECK_REPORT | DDS_CHECK_EXIT);
+    printf ("ret : %d\n", ret);
+    for (int i = 0; i < ret; i++) {
+        if (cm_infos[i].valid_data) {
+            DDS_CMParticipantBuiltinTopicData *cm = (DDS_CMParticipantBuiltinTopicData*)cm_samples[i];
+            printf("cm  : %x.%x.%x", cm->key[0], cm->key[1], cm->key[2]);
+        }
+    }
+
+
+    dds_sleepfor (DDS_SECS (5));
+
     /* Free the data location. */
     HelloWorldData_Msg_free (samples[0], DDS_FREE_ALL);
+
+    /* Clear array of samples. */
+    for (int i = 0; i < CM_SAMPLES; i++)
+    {
+        DDS_CMParticipantBuiltinTopicData_free (cm_samples[i], DDS_FREE_ALL);
+    }
 
     /* Deleting the participant will delete all its children recursively as well. */
     ret = dds_delete (participant);
