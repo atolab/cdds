e13c5e6f48dfc6034c992c089f473d69009442c4
Martin Bremmer <martin.bremmer@prismtech.com>
2017-09-18 15:06:39 +0200

    CHAM-347: Helloworld builtin test.

--- examples/helloworld/subscriber.c
+++ examples/helloworld/subscriber.c
@@ -21,6 +21,15 @@ int main (int argc, char ** argv)
     dds_return_t ret;
     dds_qos_t *qos;
 
+    dds_entity_t dcps_topic;
+    dds_entity_t dcps_reader;
+    void *dcps_samples[CM_SAMPLES];
+    dds_sample_info_t dcps_infos[CM_SAMPLES];
+    for (int i = 0; i < CM_SAMPLES; i++)
+    {
+        dcps_samples[i] = DDS_ParticipantBuiltinTopicData__alloc ();
+    }
+
     dds_entity_t cm_topic;
     dds_entity_t cm_reader;
     void *cm_samples[CM_SAMPLES];
@@ -35,10 +44,26 @@ int main (int argc, char ** argv)
     DDS_ERR_CHECK (participant, DDS_CHECK_REPORT | DDS_CHECK_EXIT);
 
     /* Create a Topic. */
-    cm_topic = dds_create_topic (participant, &DDS_CMParticipantBuiltinTopicData_desc, "CMParticipant", NULL, NULL);
+    //cm_topic = dds_create_topic (participant, &DDS_CMParticipantBuiltinTopicData_desc, "CMParticipant", NULL, NULL);
+    dcps_topic = dds_find_topic(participant, "DCPSParticipant");
+    DDS_ERR_CHECK (dcps_topic, DDS_CHECK_REPORT | DDS_CHECK_EXIT);
+    {
+        char name[100];
+        dds_get_type_name(dcps_topic, name, 100);
+        printf("Typename: %s\n", name);
+    }
+
+    cm_topic = dds_find_topic(participant, "CMParticipant");
     DDS_ERR_CHECK (cm_topic, DDS_CHECK_REPORT | DDS_CHECK_EXIT);
+    {
+        char name[100];
+        dds_get_type_name(cm_topic, name, 100);
+        printf("Typename: %s\n", name);
+    }
 
     /* Create a Reader. */
+    dcps_reader = dds_create_reader (participant, dcps_topic, NULL, NULL);
+    DDS_ERR_CHECK (dcps_reader, DDS_CHECK_REPORT | DDS_CHECK_EXIT);
     cm_reader = dds_create_reader (participant, cm_topic, NULL, NULL);
     DDS_ERR_CHECK (cm_reader, DDS_CHECK_REPORT | DDS_CHECK_EXIT);
 
@@ -52,7 +77,6 @@ int main (int argc, char ** argv)
     dds_qset_reliability (qos, DDS_RELIABILITY_RELIABLE, DDS_SECS (10));
     reader = dds_create_reader (participant, topic, qos, NULL);
     DDS_ERR_CHECK (reader, DDS_CHECK_REPORT | DDS_CHECK_EXIT);
-    dds_qos_delete(qos);
 
     printf ("\n=== [Subscriber] Waiting for a sample ...\n");
 
@@ -83,6 +107,7 @@ int main (int argc, char ** argv)
             dds_sleepfor (DDS_MSECS (20));
         }
     }
+    dds_qos_delete(qos);
 
     ret = dds_read (cm_reader, cm_samples, cm_infos, CM_SAMPLES, CM_SAMPLES);
     DDS_ERR_CHECK (ret, DDS_CHECK_REPORT | DDS_CHECK_EXIT);
@@ -90,7 +115,19 @@ int main (int argc, char ** argv)
     for (int i = 0; i < ret; i++) {
         if (cm_infos[i].valid_data) {
             DDS_CMParticipantBuiltinTopicData *cm = (DDS_CMParticipantBuiltinTopicData*)cm_samples[i];
-            printf("cm  : %x.%x.%x", cm->key[0], cm->key[1], cm->key[2]);
+            printf("cm  : %x.%x.%x\n", cm->key[0], cm->key[1], cm->key[2]);
+            printf("prod: %s\n", cm->product.value);
+        }
+    }
+
+    ret = dds_read (dcps_reader, dcps_samples, dcps_infos, CM_SAMPLES, CM_SAMPLES);
+    DDS_ERR_CHECK (ret, DDS_CHECK_REPORT | DDS_CHECK_EXIT);
+    printf ("ret : %d\n", ret);
+    for (int i = 0; i < ret; i++) {
+        if (dcps_infos[i].valid_data) {
+            DDS_ParticipantBuiltinTopicData *dcps = (DDS_ParticipantBuiltinTopicData*)dcps_samples[i];
+            printf("dcps: %x.%x.%x\n", dcps->key[0], dcps->key[1], dcps->key[2]);
+            printf("user: %s\n", dcps->user_data.value._buffer);
         }
     }
 
@@ -99,6 +136,11 @@ int main (int argc, char ** argv)
 
     /* Free the data location. */
     HelloWorldData_Msg_free (samples[0], DDS_FREE_ALL);
+
+    for (int i = 0; i < CM_SAMPLES; i++)
+    {
+        DDS_CMParticipantBuiltinTopicData_free (dcps_samples[i], DDS_FREE_ALL);
+    }
 
     /* Clear array of samples. */
     for (int i = 0; i < CM_SAMPLES; i++)
