40f8e07dc89e0eb4793067ab7b9734c2c68d0a83
Nondini Das <nondini.das@prismtech.com>
2017-10-25 14:22:30 -0400

    CHAM-458 [pubsub] Formatted white spaces in common.h, porting.c, porting.h and tglib.h files

--- tools/pubsub/porting.c
+++ tools/pubsub/porting.c
@@ -1,9 +1,9 @@
 //
-//  porting.c
-//  ospli-osplo
+//    porting.c
+//    ospli-osplo
 //
-//  Created by Erik Boasson on 11-02-2015.
-//  Copyright (c) 2015 PrismTech. All rights reserved.
+//    Created by Erik Boasson on 11-02-2015.
+//    Copyright (c) 2015 PrismTech. All rights reserved.
 //
 
 #include <stdio.h>
@@ -11,20 +11,19 @@
 #include "porting.h"
 
 #if NEED_STRSEP
-char *strsep (char **str, const char *sep)
-{
-  char *ret;
-  if (*str == NULL)
-    return NULL;
-  ret = *str;
-  while (**str && strchr (sep, **str) == 0)
-    (*str)++;
-  if (**str == '\0') {
-    *str = NULL;
-  } else {
-    **str = '\0';
-    (*str)++;
-  }
-  return ret;
+char *strsep(char **str, const char *sep) {
+    char *ret;
+    if (*str == NULL)
+        return NULL;
+    ret = *str;
+    while (**str && strchr(sep, **str) == 0)
+        (*str)++;
+    if (**str == '\0') {
+        *str = NULL;
+    } else {
+        **str = '\0';
+        (*str)++;
+    }
+    return ret;
 }
 #endif
