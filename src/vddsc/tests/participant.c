b328cd12e51930a1e628600e4605700d65d223b5
¨Kurtulus <kurtulus.oksuztepe@adlinktech.com>
2017-10-25 10:37:12 +0200

    CHAM-399 invalid read failures were fixed by giving static array to putenv

--- src/vddsc/tests/participant.c
+++ src/vddsc/tests/participant.c
@@ -110,13 +110,16 @@ Test(vddsc_participant, create_with_no_conf_no_env) {
   cr_assert_status_eq(status, DDS_RETCODE_OK, "dds_get_domainid(participant, domain_id)");
   cr_assert_eq(domain_id, valid_domain, "Retrieved domain ID must be valid");
 
-
   //DDS_DOMAIN_DEFAULT with no domain environment variable
   participant3 = dds_create_participant (DDS_DOMAIN_DEFAULT, NULL, NULL);
   cr_assert_gt(participant3, 0, "Valid participant must be received for DDS_DOMAIN_DEFAULT with no domain environment variable");
   status = dds_get_domainid(participant3, &domain_id);
   cr_assert_status_eq(status, DDS_RETCODE_OK, "dds_get_domainid(participant, domain_id)");
   cr_assert_eq(domain_id, valid_domain, "Retrieved domain ID must be valid");
+
+  dds_delete(participant2);
+  dds_delete(participant3);
+
 
 }
 
@@ -241,13 +244,11 @@ Test(vddsc_participant, create_with_conf_no_env) {
   dds_domainid_t domain_id;
   dds_domainid_t valid_domain=3;
 
-  char *env_uri_str;
-  env_uri_str = os_malloc(strlen("VORTEXDDS_URI") + strlen("=") + strlen(CONFIG_ENV_SIMPLE_UDP) + 1);
+  static char env_uri_str[1000];
   (void) sprintf(env_uri_str, "%s=%s", "VORTEXDDS_URI", CONFIG_ENV_SIMPLE_UDP);
   os_putenv(env_uri_str);
 
-  char *env_mp_str;
-  env_mp_str = os_malloc(strlen("MAX_PARTICIPANTS") + strlen("=") + strlen(CONFIG_ENV_MAX_PARTICIPANTS) + 1);
+  static char env_mp_str[100];
   (void) sprintf(env_mp_str, "%s=%s", "MAX_PARTICIPANTS", CONFIG_ENV_MAX_PARTICIPANTS);
   os_putenv(env_mp_str);
 
@@ -280,8 +281,6 @@ Test(vddsc_participant, create_with_conf_no_env) {
   dds_delete(participant2);
   dds_delete(participant3);
 
-  os_free(env_uri_str);
-  os_free(env_mp_str);
 
 }
 
@@ -293,13 +292,11 @@ Test(vddsc_participant, create_with_conf_valid_env) {
   dds_domainid_t domain_id;
   dds_domainid_t valid_domain=2;
 
-  char *env_uri_str;
-  env_uri_str = os_malloc(strlen("VORTEXDDS_URI") + strlen("=") + strlen(CONFIG_ENV_SIMPLE_UDP) + 1);
+  static char env_uri_str[1000];
   (void) sprintf(env_uri_str, "%s=%s", "VORTEXDDS_URI", CONFIG_ENV_SIMPLE_UDP);
   os_putenv(env_uri_str);
 
-  char *env_mp_str;
-  env_mp_str = os_malloc(strlen("MAX_PARTICIPANTS") + strlen("=") + strlen(CONFIG_ENV_MAX_PARTICIPANTS) + 1);
+  static char env_mp_str[100];
   (void) sprintf(env_mp_str, "%s=%s", "MAX_PARTICIPANTS", CONFIG_ENV_MAX_PARTICIPANTS);
   os_putenv(env_mp_str);
 
@@ -335,9 +332,6 @@ Test(vddsc_participant, create_with_conf_valid_env) {
   dds_delete (participant2);
   dds_delete (participant5);
 
-  os_free(env_uri_str);
-  os_free(env_mp_str);
-
 }
 
 Test(vddsc_participant, create_with_conf_invalid_env)
@@ -350,15 +344,14 @@ Test(vddsc_participant, create_with_conf_invalid_env)
   dds_domainid_t valid_domain=3;
   putenv("VORTEX_DOMAIN=-5");
 
-  char *env_uri_str;
-  env_uri_str = os_malloc(strlen("VORTEXDDS_URI") + strlen("=") + strlen(CONFIG_ENV_SIMPLE_UDP) + 1);
+  static char env_uri_str[1000];
   (void) sprintf(env_uri_str, "%s=%s", "VORTEXDDS_URI", CONFIG_ENV_SIMPLE_UDP);
   os_putenv(env_uri_str);
 
-  char *env_mp_str;
-  env_mp_str = os_malloc(strlen("MAX_PARTICIPANTS") + strlen("=") + strlen(CONFIG_ENV_MAX_PARTICIPANTS) + 1);
+  static char env_mp_str[100];
   (void) sprintf(env_mp_str, "%s=%s", "MAX_PARTICIPANTS", CONFIG_ENV_MAX_PARTICIPANTS);
   os_putenv(env_mp_str);
+
   const char * env_uri = os_getenv("VORTEXDDS_URI");
   const char * env_domain = os_getenv("VORTEX_DOMAIN");
   dds_domainid_t env_domain_value = atoi (env_domain);
@@ -379,10 +372,6 @@ Test(vddsc_participant, create_with_conf_invalid_env)
   participant4 = dds_create_participant (DDS_DOMAIN_DEFAULT, NULL, NULL);
   cr_assert_lt(participant4, 0, "Invalid participant  must be received for DDS_DOMAIN_DEFAULT with invalid domain environment variable");
 
-
-  os_free(env_mp_str);
-  os_free(env_uri_str);
-
 }
 
 Test(vddsc_participant, create_with_conf_default_env)
@@ -394,13 +383,11 @@ Test(vddsc_participant, create_with_conf_default_env)
   dds_domainid_t valid_domain=3; //as in config file
   putenv("VORTEX_DOMAIN=-1"); //DDS_DOMAIN_DEFAULT
 
-  char *env_uri_str;
-  env_uri_str = os_malloc(strlen("VORTEXDDS_URI") + strlen("=") + strlen(CONFIG_ENV_SIMPLE_UDP) + 1);
+  static char env_uri_str[1000];
   (void) sprintf(env_uri_str, "%s=%s", "VORTEXDDS_URI", CONFIG_ENV_SIMPLE_UDP);
   os_putenv(env_uri_str);
 
-  char *env_mp_str;
-  env_mp_str = os_malloc(strlen("MAX_PARTICIPANTS") + strlen("=") + strlen(CONFIG_ENV_MAX_PARTICIPANTS) + 1);
+  static char env_mp_str[100];
   (void) sprintf(env_mp_str, "%s=%s", "MAX_PARTICIPANTS", CONFIG_ENV_MAX_PARTICIPANTS);
   os_putenv(env_mp_str);
 
@@ -431,11 +418,6 @@ Test(vddsc_participant, create_with_conf_default_env)
 
   dds_delete (participant2);
   dds_delete (participant4);
-
-  os_free(env_uri_str);
-  os_free(env_mp_str);
-
-
 
 }
 
