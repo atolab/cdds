7bf30d4694082eb2d9c1fbafa661c2dc38e9d4b2
gabrik <gabriele.baldoni@gmail.com>
2018-05-17 13:11:01 +0200

    updated helloworld example to not be oneshot

--- src/examples/helloworld/publisher.c
+++ src/examples/helloworld/publisher.c
@@ -2,14 +2,30 @@
 #include "HelloWorldData.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+dds_entity_t participant;
+dds_return_t ret;
+
+void intHandler(int dummy) {
+
+    /* Deleting the participant will delete all its children recursively as well. */
+    ret = dds_delete (participant);
+    DDS_ERR_CHECK (ret, DDS_CHECK_REPORT | DDS_CHECK_EXIT);
+
+    exit(EXIT_SUCCESS);
+}
+
 
 int main (int argc, char ** argv)
 {
-    dds_entity_t participant;
+
     dds_entity_t topic;
     dds_entity_t writer;
-    dds_return_t ret;
     HelloWorldData_Msg msg;
+
+    /* Handling CTRL-C */
+    signal(SIGINT, intHandler);
 
     /* Create a Participant. */
     participant = dds_create_participant (DDS_DOMAIN_DEFAULT, NULL, NULL);
@@ -30,30 +46,32 @@ int main (int argc, char ** argv)
 
     while(true)
     {
-      uint32_t status;
-      ret = dds_get_status_changes (writer, &status);
-      DDS_ERR_CHECK (ret, DDS_CHECK_REPORT | DDS_CHECK_EXIT);
+        uint32_t status;
+        ret = dds_get_status_changes (writer, &status);
+        DDS_ERR_CHECK (ret, DDS_CHECK_REPORT | DDS_CHECK_EXIT);
 
-      if (status == DDS_PUBLICATION_MATCHED_STATUS) {
-        break;
-      }
-      /* Polling sleep. */
-      dds_sleepfor (DDS_MSECS (20));
+        if (status == DDS_PUBLICATION_MATCHED_STATUS) {
+            break;
+        }
+        /* Polling sleep. */
+        dds_sleepfor (DDS_MSECS (20));
     }
 
     /* Create a message to write. */
     msg.userID = 1;
     msg.message = "Hello World";
 
-    printf ("=== [Publisher]  Writing : ");
-    printf ("Message (%d, %s)\n", msg.userID, msg.message);
+    while (true) {
+        printf ("=== [Publisher]  Writing : ");
+        printf ("Message (%d, %s)\n", msg.userID, msg.message);
 
-    ret = dds_write (writer, &msg);
-    DDS_ERR_CHECK (ret, DDS_CHECK_REPORT | DDS_CHECK_EXIT);
+        ret = dds_write (writer, &msg);
+        DDS_ERR_CHECK (ret, DDS_CHECK_REPORT | DDS_CHECK_EXIT);
+        msg.userID++;
+        sleep(3);
 
-    /* Deleting the participant will delete all its children recursively as well. */
-    ret = dds_delete (participant);
-    DDS_ERR_CHECK (ret, DDS_CHECK_REPORT | DDS_CHECK_EXIT);
+    }
 
-    return EXIT_SUCCESS;
+
+
 }
--- src/examples/helloworld/subscriber.c
+++ src/examples/helloworld/subscriber.c
@@ -4,19 +4,36 @@
 #include <string.h>
 #include <stdlib.h>
 
+#include <signal.h>
+
+dds_entity_t participant;
+dds_return_t ret;
+void intHandler(int dummy) {
+
+    /* Deleting the participant will delete all its children recursively as well. */
+    ret = dds_delete (participant);
+    DDS_ERR_CHECK (ret, DDS_CHECK_REPORT | DDS_CHECK_EXIT);
+
+    exit(EXIT_SUCCESS);
+}
+
 /* An array of one message (aka sample in dds terms) will be used. */
 #define MAX_SAMPLES 1
 
+
 int main (int argc, char ** argv)
 {
-    dds_entity_t participant;
+
     dds_entity_t topic;
     dds_entity_t reader;
     HelloWorldData_Msg *msg;
     void *samples[MAX_SAMPLES];
     dds_sample_info_t infos[MAX_SAMPLES];
-    dds_return_t ret;
+
     dds_qos_t *qos;
+
+    /* Handling CTRL-C */
+    signal(SIGINT, intHandler);
 
     /* Create a Participant. */
     participant = dds_create_participant (DDS_DOMAIN_DEFAULT, NULL, NULL);
@@ -38,14 +55,15 @@ int main (int argc, char ** argv)
 
     /* Initialize sample buffer, by pointing the void pointer within
      * the buffer array to a valid sample memory location. */
-    samples[0] = HelloWorldData_Msg__alloc ();
+    //samples[0] = HelloWorldData_Msg__alloc ();
 
     /* Poll until data has been read. */
     while (true)
     {
+        samples[0] = HelloWorldData_Msg__alloc ();
         /* Do the actual read.
          * The return value contains the number of read samples. */
-        ret = dds_read (reader, samples, infos, MAX_SAMPLES, MAX_SAMPLES);
+        ret =  dds_take(reader, samples, infos, MAX_SAMPLES, MAX_SAMPLES);
         DDS_ERR_CHECK (ret, DDS_CHECK_REPORT | DDS_CHECK_EXIT);
 
         /* Check if we read some data and it is valid. */
@@ -55,13 +73,16 @@ int main (int argc, char ** argv)
             msg = (HelloWorldData_Msg*) samples[0];
             printf ("=== [Subscriber] Received : ");
             printf ("Message (%d, %s)\n", msg->userID, msg->message);
-            break;
+
+            /* Free the data location. */
+            HelloWorldData_Msg_free (samples[0], DDS_FREE_ALL);
         }
         else
         {
             /* Polling sleep. */
             dds_sleepfor (DDS_MSECS (20));
         }
+
     }
 
     /* Free the data location. */
